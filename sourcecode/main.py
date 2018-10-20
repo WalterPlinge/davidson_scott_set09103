8a8c9c02c8e8e254d362565aca61a5f52bd72e67
Walter Plinge <22519813+WalterPlinge@users.noreply.github.com>
2018-10-20 19:49:11 +0100

    Update

--- sourcecode/main.py
+++ sourcecode/main.py
@@ -1,6 +1,7 @@
 import ConfigParser
 import logging
 import os
+import random
 import sqlite3
 
 from flask import abort, flash, Flask, g, json, redirect, render_template, request, session, url_for
@@ -9,53 +10,149 @@ from logging.handlers import RotatingFileHandler
 app = Flask(__name__)
 app.secret_key = os.urandom(64)
 
-json_genre = json.load(open("data/genre.json"))
-json_tracks = json.load(open("data/tracks.json"))
+json_albums = json.load(open("data/album.json"))['albums']
+json_artists = json.load(open("data/artist.json"))['artists']
+json_genres = json.load(open("data/genre.json"))['genres']
+json_tracks = json.load(open("data/tracks.json"))['tracks']
 
 @app.route('/')
 def index():
 	this_route = url_for('.index')
 	app.logger.info('Logging a test message from ' + this_route)
-	return render_template('index.html')
+
+	genre = random.choice(json_genres)['title']
+	artist = random.choice(json_artists)['title']
+	album = random.choice(json_albums)['title']
+	track = random.choice(json_tracks)['title']
+
+	return render_template('index.html', pagetitle='Musix', genre=genre, artist=artist, album=album, track=track)
 
 @app.route('/album/')
-@app.route('/album/<album>')
-def album(album=None):
-	if album == None:
-		return 'Albums:'
-	return 'album = ' + album
+@app.route('/album/<urlalbum>')
+def album(urlalbum=None):
+	this_route = url_for('.album')
+	app.logger.info('Logging a request from ' + this_route)
+
+	if urlalbum == None:
+		albums = []
+		for track in json_tracks:
+			for album in track['albums']:
+				albums.append(album)
+
+		return render_template('album.html', albums=list(set(albums)))
+
+	else:
+		for album in json_albums:
+			if album['title'].lower() == urlalbum.lower():
+
+				genres = []
+				tracks = []
+
+				for track in json_tracks:
+					for a in track['albums']:
+						if a.lower() == urlalbum.lower():
+
+							tracks.append(track['title'])
+
+							for genre in track['genres']:
+								genres.append(genre)
+
+				return render_template('album.html', album=album, genres=list(set(genres)), tracks=list(set(tracks)))
+
+	abort(404)
 
 @app.route('/artist/')
-@app.route('/artist/<artist>')
-def artist(artist=None):
-	if artist == None:
-		return 'Artists:'
-	return 'artist = ' + artist
+@app.route('/artist/<urlartist>')
+def artist(urlartist=None):
+	this_route = url_for('.artist')
+	app.logger.info('Logging a request from ' + this_route)
+
+	if urlartist == None:
+		artists = []
+		for track in json_tracks:
+			for artist in track['artist']:
+				artists.append(artist)
+
+		return render_template('artist.html', artists=list(set(artists)))
+
+	else:
+		for artist in json_artists:
+			if artist['title'].lower() == urlartist.lower():
+
+				genres = []
+				albums = []
+				tracks = []
+
+				for track in json_tracks:
+					if track['artist'].lower() == urlartist.lower():
+
+						tracks.append(track['title'])
+
+						for album in track['albums']:
+							albums.append(album)
+
+						for genre in track['genres']:
+							genres.append(genre)
+
+				return render_template('artist.html', artist=artist, albums=list(set(albums)), genres=list(set(genres)), tracks=list(set(tracks)))
+
+	abort(404)
 
 @app.route('/genre/')
-@app.route('/genre/<genre>')
-def genre(genre=None):
+@app.route('/genre/<urlgenre>')
+def genre(urlgenre=None):
 	this_route = url_for('.genre')
-	app.logger.info('Logging a test message from ' + this_route)
-	if genre == None:
-		genres = json_genre["genre"]
-		return render_template('genre.html', genres=genres)
+	app.logger.info('Logging a request from ' + this_route)
+
+	if urlgenre == None:
+		genres = []
+		for track in json_tracks:
+			for genre in track['genres']:
+				genres.append(genre)
+
+		return render_template('genre.html', genres=list(set(genres)))
 	else:
-		return render_template('genre.html')
+		for genre in json_genres:
+			if genre['title'].lower() == urlgenre.lower():
+
+				albums = []
+				artists = []
+				tracks = []
+
+				for track in json_tracks:
+					for g in track['genres']:
+						if g.lower() == urlgenre.lower():
+
+							artists.append(track['artist'])
+
+							tracks.append(track['title'])
+
+							for a in track['albums']:
+								albums.append(a)
+
+				return render_template('genre.html', genre=genre, albums=list(set(albums)), artists=list(set(artists)), tracks=list(set(tracks)))
+
+	abort(404)
 
 @app.route('/track/')
-@app.route('/track/<track>')
-def track(track=None):
-	if track == None:
-		tracks = json_tracks['tracks']
-		return render_template('track.html', tracks=tracks)
+@app.route('/track/<urltrack>')
+def track(urltrack=None):
+	this_route = url_for('.track')
+	app.logger.info('Logging a request from ' + this_route)
+
+	if urltrack == None:
+		tracks = []
+		for track in json_tracks:
+			tracks.append(track)
+
+		return render_template('track.html', tracks=list(set(tracks)))
+
 	else:
-		tracks = json_tracks['tracks']
-		for t in tracks:
-			n = t['title']
-			if n.lower() == track.lower():
-				return render_template('track.html', track=t)
-		abort(404)
+		for track in json_tracks:
+			if track['title'].lower() == urltrack.lower():
+				return render_template('track.html', track=track)
+
+	abort(404)
 
 @app.route('/drseuss/')
 def drseuss():
@@ -108,7 +205,7 @@ def ttt(path=None):
 
 @app.errorhandler(404)
 def page_not_found(error):
-	return 'Cannot find the page you requested.', 404
+	return render_template('error.html')
 
 def init(app):
 	config = ConfigParser.ConfigParser()
